b69e639d0600c48034f50f9a95edc805cfed23b5
Wender Natanael <wendermarquesmarques@gmail.com>
2022-04-10 13:51:53 -0300

    atualizando

--- JS/treinos/jogo-da-forca/js/index.js
+++ JS/treinos/jogo-da-forca/js/index.js
@@ -1,60 +1,13 @@
-let palavra = 'arroz'
+let palavra = window.prompt('')
 let quantasLetras = palavra.length
 const section = document.getElementById('letras')
 
-var div1 = document.getElementById('d1')
-var div2 = document.getElementById('d2')
-var div3 = document.getElementById('d3')
-var div4 = document.getElementById('d4')
-var div5 = document.getElementById('d5')
-var div6 = document.getElementById('d6')
-var div7 = document.getElementById('d7')
-var div8 = document.getElementById('d8')
+//Vai criar uma linha pra cada letra
+for(let totalLetras = 0; totalLetras < quantasLetras; totalLetras +=1) {
+    let div = document.createElement('div')
+    section.appendChild(div)
+}
 
-if(quantasLetras === 1) {
-    div1.style.display = 'block'
-} else if(quantasLetras === 2) {
-    div1.style.display = 'block'
-    div2.style.display = 'block'
-} else if(quantasLetras === 3) {
-    div1.style.display = 'block'
-    div2.style.display = 'block'
-    div3.style.display = 'block'
-} else if(quantasLetras === 4) {
-    div1.style.display = 'block'
-    div2.style.display = 'block'
-    div3.style.display = 'block'
-    div4.style.display = 'block'
-} else if(quantasLetras === 5) {
-    div1.style.display = 'block'
-    div2.style.display = 'block'
-    div3.style.display = 'block'
-    div4.style.display = 'block'
-    div5.style.display = 'block'
-} else if(quantasLetras === 6) {
-    div1.style.display = 'block'
-    div2.style.display = 'block'
-    div3.style.display = 'block'
-    div4.style.display = 'block'
-    div5.style.display = 'block'
-    div6.style.display = 'block'
-} else if(quantasLetras === 7) {
-    div1.style.display = 'block'
-    div2.style.display = 'block'
-    div3.style.display = 'block'
-    div4.style.display = 'block'
-    div5.style.display = 'block'
-    div6.style.display = 'block'
-    div7.style.display = 'block'
-} else if(quantasLetras === 8) {
-    div1.style.display = 'block'
-    div2.style.display = 'block'
-    div3.style.display = 'block'
-    div4.style.display = 'block'
-    div5.style.display = 'block'
-    div6.style.display = 'block'
-    div7.style.display = 'block'
-    div8.style.display = 'block'
-} 
+
 
 
