ae45b3e69a0bf60f429abcf91e6dadf267bfbb39
Wender Natanael <wendermarquesmarques@gmail.com>
2022-04-10 16:05:59 -0300

    novo joguinho em Js

---
+++ JS/treinos/jogo-da-forca/js/index.js
@@ -0,0 +1,60 @@
+let palavra = 'arroz'
+let quantasLetras = palavra.length
+const section = document.getElementById('letras')
+
+var div1 = document.getElementById('d1')
+var div2 = document.getElementById('d2')
+var div3 = document.getElementById('d3')
+var div4 = document.getElementById('d4')
+var div5 = document.getElementById('d5')
+var div6 = document.getElementById('d6')
+var div7 = document.getElementById('d7')
+var div8 = document.getElementById('d8')
+
+if(quantasLetras === 1) {
+    div1.style.display = 'block'
+} else if(quantasLetras === 2) {
+    div1.style.display = 'block'
+    div2.style.display = 'block'
+} else if(quantasLetras === 3) {
+    div1.style.display = 'block'
+    div2.style.display = 'block'
+    div3.style.display = 'block'
+} else if(quantasLetras === 4) {
+    div1.style.display = 'block'
+    div2.style.display = 'block'
+    div3.style.display = 'block'
+    div4.style.display = 'block'
+} else if(quantasLetras === 5) {
+    div1.style.display = 'block'
+    div2.style.display = 'block'
+    div3.style.display = 'block'
+    div4.style.display = 'block'
+    div5.style.display = 'block'
+} else if(quantasLetras === 6) {
+    div1.style.display = 'block'
+    div2.style.display = 'block'
+    div3.style.display = 'block'
+    div4.style.display = 'block'
+    div5.style.display = 'block'
+    div6.style.display = 'block'
+} else if(quantasLetras === 7) {
+    div1.style.display = 'block'
+    div2.style.display = 'block'
+    div3.style.display = 'block'
+    div4.style.display = 'block'
+    div5.style.display = 'block'
+    div6.style.display = 'block'
+    div7.style.display = 'block'
+} else if(quantasLetras === 8) {
+    div1.style.display = 'block'
+    div2.style.display = 'block'
+    div3.style.display = 'block'
+    div4.style.display = 'block'
+    div5.style.display = 'block'
+    div6.style.display = 'block'
+    div7.style.display = 'block'
+    div8.style.display = 'block'
+} 
+
+
