358353d17066781fe906c175d9257fbb7b969765
Wender Natanael <wendermarquesmarques@gmail.com>
2022-04-16 15:28:43 -0300

    some changes

--- JS/treinos/calculadora-js/index.js
+++ JS/treinos/calculadora-js/index.js
@@ -1,27 +1,29 @@
-var t7 = document.getElementById('t7')
-var t8 = document.getElementById('t8')
-var t9 = document.getElementById('t9')
-var t4 = document.getElementById('t4')
-var t5 = document.getElementById('t5')
-var t6 = document.getElementById('t6')
-var t1 = document.getElementById('t1')
-var t2 = document.getElementById('t2')
-var t3 = document.getElementById('t3')
-var t0 = document.getElementById('t0')
-var tponto = document.getElementById('t.')
-var tigual = document.getElementById('t=')
-var tmais = document.getElementById('t+')
-var tX = document.getElementById('tX')
-var tdivisao = document.getElementById('t/')
-var tmenos = document.getElementById('t-')
+/* eslint-disable no-undef */
+var t7 = document.getElementById("t7");
+var t8 = document.getElementById("t8");
+var t9 = document.getElementById("t9");
+var t4 = document.getElementById("t4");
+var t5 = document.getElementById("t5");
+var t6 = document.getElementById("t6");
+var t1 = document.getElementById("t1");
+var t2 = document.getElementById("t2");
+var t3 = document.getElementById("t3");
+var t0 = document.getElementById("t0");
+var tponto = document.getElementById("t.");
+var tigual = document.getElementById("t=");
+var tmais = document.getElementById("t+");
+var tX = document.getElementById("tX");
+var tdivisao = document.getElementById("t/");
+var tmenos = document.getElementById("t-");
 
-const tela = document.getElementById('tela')
-const teclado = document.getElementById('teclado')
-let num1 = {}
-let num2 = {}
+const tela = document.getElementById("tela");
+const teclado = document.getElementById("teclado");
+let num1 = {};
+let num2 = {};
 
-let ativo = false
-let num1Ativo = true
-let UmOuDois = 1
-let sinal
+let ativo = false;
+let num1Ativo = true;
+let UmOuDois = 1;
+let sinal;
+
 
