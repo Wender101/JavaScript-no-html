4a0e6a9b929d67045a2388cd17932bf57f2981e5
Wender Natanael <wendermarquesmarques@gmail.com>
2022-06-29 15:59:02 -0300

    terminado a carteirinha

---
+++ JS/Segundo-curso-de-js/aula89/index.js
@@ -0,0 +1,8 @@
+const pessoa = {
+    nome: 'Luiz',
+    sobrenome: 'Otávio'
+}
+
+const chave = 'nome'
+console.log(pessoa[chave]); // É uma forma dinâmica de pegar um valor do obj, assim eu posso ultilizar uma var
+console.log(pessoa['sobrenome']);
