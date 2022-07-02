870b55f748a5f98dfa5afa8781f204fd489ebba7
Wender Natanael <wendermarquesmarques@gmail.com>
2022-07-02 11:55:03 -0300

    curso de js

--- JS/Segundo-curso-de-js/aula91/index.js
+++ JS/Segundo-curso-de-js/aula91/index.js
@@ -5,12 +5,18 @@ function Produto(nome, preco, estoque) {
 
     Object.defineProperty(this, 'estoque', {
         enumerable: true,
-        value: true,
+        configurable: true,
        get: () => {
-        return estoqeu;
+        return estoque;
+       }, 
+
+       set: valor => {
+        
        }
     })
 }
 
 const p1 = new Produto('Camisa', 20, 3)
 console.log(p1)
+p1.estoque = 'O valor que eu quero'
+console.log(p1.estoque)
