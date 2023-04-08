a9b9c7ed13c418e7dcf5a1dc96dffb0dba8f1334
Wender Natanael <93059850+Wender101@users.noreply.github.com>
2023-04-08 12:18:41 -0300

    Delete JS/treinos/MusicPlayer directory

--- JS/treinos/MusicPlayer/assets/js/cadastrar.js
+++
@@ -1,7 +0,0 @@
-document.querySelector('button').addEventListener('click', () => {
-    let inputNome = document.querySelector('input').value
-
-    if(inputNome.length > 0) {
-       login()
-    }
-})
