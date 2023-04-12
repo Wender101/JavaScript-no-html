2fea21859a6d0c262b39136929c65db28e00a88d
Wender Natanael <93059850+Wender101@users.noreply.github.com>
2023-04-12 13:19:30 -0300

    Usando .then para melhorar as funções

--- JS/treinos/MusicPlayer/assets/js/favoritarMusicas.js
+++ JS/treinos/MusicPlayer/assets/js/favoritarMusicas.js
@@ -8,28 +8,40 @@ function favoritarMusica(musicaFavoritada) {
 
             if(Usuarios.infUser.Email == email) {
 
-                for(let c = 0; c < Usuarios.Musica.MusicasCurtidas.length; c++) {
-                    if(Usuarios.Musica.MusicasCurtidas[c].LinkImgiMusica == musicaFavoritada.LinkImgiMusica && Usuarios.Musica.MusicasCurtidas[c].LinkAudio == musicaFavoritada.LinkAudio && Usuarios.Musica.MusicasCurtidas[c].EmailUser == musicaFavoritada.EmailUser && Usuarios.Musica.MusicasCurtidas[c].Nome == musicaFavoritada.Nome && feito == false) {
-                        feito = true
-                        jaTemEssaMusicaNosFavoritos = true
-
-                        let musica = Usuarios.Musica
-                        musica.MusicasCurtidas.splice(c, 1)
-
-                        db.collection('Usuarios').doc(valor.id).update({Musica: musica})
-                    }
+                function checarMusica() {
+                    return new Promise((resolve, reject) => {
+                        if(Usuarios.Musica.MusicasCurtidas.length > 0) {
+                            for(let c = 0; c < Usuarios.Musica.MusicasCurtidas.length; c++) {
+                                if(Usuarios.Musica.MusicasCurtidas[c].LinkImgiMusica == musicaFavoritada.LinkImgiMusica && Usuarios.Musica.MusicasCurtidas[c].LinkAudio == musicaFavoritada.LinkAudio && Usuarios.Musica.MusicasCurtidas[c].EmailUser == musicaFavoritada.EmailUser && Usuarios.Musica.MusicasCurtidas[c].Nome == musicaFavoritada.Nome && feito == false) {
+                                    //? Caso já tenha a música add nos favoritos
+                                    feito = true
+                                    resolve(c)
+    
+                                } else if(c + 1 == Usuarios.Musica.MusicasCurtidas.length && feito == false) {
+                                    feito = true
+                                    //? Caso não tenha a música add nos favoritos
+                                    reject('Não tem a música')
+                                }
+                            }
+                        } else {
+                            reject('Não tem a música')
+                        }
+                    })
                 }
 
-                setTimeout(() => {
-                    if(jaTemEssaMusicaNosFavoritos == false) {
-                        jaTemEssaMusicaNosFavoritos = true
-                        feito = true
-                        let musica = Usuarios.Musica
-                        musica.MusicasCurtidas.push(musicaFavoritada)
+                let feito2 = false
+                checarMusica().then((resolve) => {
+                    feito2 = true
+                    let musica = Usuarios.Musica
+                    musica.MusicasCurtidas.splice(resolve, 1)
+                    db.collection('Usuarios').doc(valor.id).update({Musica: musica})
 
-                        db.collection('Usuarios').doc(valor.id).update({Musica: musica})
-                    }
-                }, 200)
+                }).catch((reject) => {
+                    feito2 = true
+                    let musica = Usuarios.Musica
+                    musica.MusicasCurtidas.push(musicaFavoritada)
+                    db.collection('Usuarios').doc(valor.id).update({Musica: musica})
+                })                
             }
         })
     })
@@ -53,13 +65,28 @@ function cehcarFavoritos(musicaChecar, heart) {
                     }
                 }
 
-                setTimeout(() => {
-                    if(jaTemEssaMusicaNosFavoritos == false) {
-                        jaTemEssaMusicaNosFavoritos = true
-                        feito = true
-                        heart.src = 'assets/img/icones/icon _heart_ (1).png'
-                    }
-                }, 200)
+                function checarMusica() {
+                    return new Promise((resolve, reject) => {
+
+                        for(let c = 0; c < Usuarios.Musica.MusicasCurtidas.length; c++) {
+                            if(Usuarios.Musica.MusicasCurtidas[c].LinkImgiMusica == musicaChecar.LinkImgiMusica && Usuarios.Musica.MusicasCurtidas[c].LinkAudio == musicaChecar.LinkAudio && Usuarios.Musica.MusicasCurtidas[c].EmailUser == musicaChecar.EmailUser && Usuarios.Musica.MusicasCurtidas[c].NomeMusica == musicaChecar.NomeMusica && feito == false) {
+                                feito = true
+                                resolve('Tem a música')
+
+                            } else if(c + 1 == Usuarios.Musica.MusicasCurtidas.length && feito == false) {
+                                feito = true
+                                reject('Não tem a música')
+                            }
+                        }
+                    })
+                }
+
+                checarMusica().then((resolve) => {
+                    heart.src = 'assets/img/icones/icon _heart_.png'
+
+                }).catch((reject) => {
+                    heart.src = 'assets/img/icones/icon _heart_ (1).png'
+                })                
             }
         })
     })
